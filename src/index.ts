5050b37879152cf3c03cb8b13364a5c4a36e719b
vorapong sangpiyaphet <vorapong.san@mahidol.ac.th>
2024-04-01 09:27:13 +0700

    feat: add function for convert Snellen to LogMAR value

--- src/index.ts
+++ src/index.ts
@@ -6,41 +6,43 @@
  */
 interface VisualAcuityScale {
   display: string;
+  snellenNumerator: number;
+  snellenDenominator: number;
   LogMAR: number;
 }
 
 const visualAcuityScales: Record<'foot' | 'metre', VisualAcuityScale[]> = {
   'foot': [
-    { display: `20/200`, LogMAR: 1.00 },
-    { display: `20/160`, LogMAR: 0.90 },
-    { display: `20/125`, LogMAR: 0.80 },
-    { display: `20/100`, LogMAR: 0.70 },
-    { display: `20/80`, LogMAR: 0.60 },
-    { display: `20/63`, LogMAR: 0.50 },
-    { display: `20/50`, LogMAR: 0.40 },
-    { display: `20/40`, LogMAR: 0.30 },
-    { display: `20/32`, LogMAR: 0.20 },
-    { display: `20/25`, LogMAR: 0.10 },
-    { display: `20/20`, LogMAR: 0.00 },
-    { display: `20/16`, LogMAR: -0.10 },
-    { display: `20/12.5`, LogMAR: -0.20 },
-    { display: `20/10`, LogMAR: -0.30 },
+    { display: `20/200`, snellenNumerator: 20, snellenDenominator: 200, LogMAR: 1.00 },
+    { display: `20/160`, snellenNumerator: 20, snellenDenominator: 160, LogMAR: 0.90 },
+    { display: `20/125`, snellenNumerator: 20, snellenDenominator: 125, LogMAR: 0.80 },
+    { display: `20/100`, snellenNumerator: 20, snellenDenominator: 100, LogMAR: 0.70 },
+    { display: `20/80`, snellenNumerator: 20, snellenDenominator: 80, LogMAR: 0.60 },
+    { display: `20/63`, snellenNumerator: 20, snellenDenominator: 63, LogMAR: 0.50 },
+    { display: `20/50`, snellenNumerator: 20, snellenDenominator: 50, LogMAR: 0.40 },
+    { display: `20/40`, snellenNumerator: 20, snellenDenominator: 40,  LogMAR: 0.30 },
+    { display: `20/32`, snellenNumerator: 20, snellenDenominator: 32, LogMAR: 0.20 },
+    { display: `20/25`, snellenNumerator: 20, snellenDenominator: 25, LogMAR: 0.10 },
+    { display: `20/20`, snellenNumerator: 20, snellenDenominator: 20, LogMAR: 0.00 },
+    { display: `20/16`, snellenNumerator: 20, snellenDenominator: 16, LogMAR: -0.10 },
+    { display: `20/12.5`, snellenNumerator: 20, snellenDenominator: 12.5, LogMAR: -0.20 },
+    { display: `20/10`, snellenNumerator: 20, snellenDenominator: 10, LogMAR: -0.30 },
   ],
   'metre': [
-    { display: `6/60`, LogMAR: 1.00 },
-    { display: `6/48`, LogMAR: 0.90 },
-    { display: `6/38`, LogMAR: 0.80 },
-    { display: `6/30`, LogMAR: 0.70 },
-    { display: `6/24`, LogMAR: 0.60 },
-    { display: `6/18`, LogMAR: 0.50 },
-    { display: `6/15`, LogMAR: 0.40 },
-    { display: `6/12`, LogMAR: 0.30 },
-    { display: `6/9.5`, LogMAR: 0.20 },
-    { display: `6/7.5`, LogMAR: 0.10 },
-    { display: `6/6`, LogMAR: 0.00 },
-    { display: `6/4.8`, LogMAR: -0.10 },
-    { display: `6/3.8`, LogMAR: -0.20 },
-    { display: `6/3`, LogMAR: -0.30 },
+    { display: `6/60`, snellenNumerator: 6, snellenDenominator: 60, LogMAR: 1.00 },
+    { display: `6/48`, snellenNumerator: 6, snellenDenominator: 48, LogMAR: 0.90 },
+    { display: `6/38`, snellenNumerator: 6, snellenDenominator: 38, LogMAR: 0.80 },
+    { display: `6/30`, snellenNumerator: 6, snellenDenominator: 30, LogMAR: 0.70 },
+    { display: `6/24`, snellenNumerator: 6, snellenDenominator: 24, LogMAR: 0.60 },
+    { display: `6/18`, snellenNumerator: 6, snellenDenominator: 18, LogMAR: 0.50 },
+    { display: `6/15`, snellenNumerator: 6, snellenDenominator: 15, LogMAR: 0.40 },
+    { display: `6/12`, snellenNumerator: 6, snellenDenominator: 12, LogMAR: 0.30 },
+    { display: `6/9.5`, snellenNumerator: 6, snellenDenominator: 9.5, LogMAR: 0.20 },
+    { display: `6/7.5`, snellenNumerator: 6, snellenDenominator: 7.5, LogMAR: 0.10 },
+    { display: `6/6`, snellenNumerator: 6, snellenDenominator: 6, LogMAR: 0.00 },
+    { display: `6/4.8`, snellenNumerator: 6, snellenDenominator: 4.8, LogMAR: -0.10 },
+    { display: `6/3.8`, snellenNumerator: 6, snellenDenominator: 3.8, LogMAR: -0.20 },
+    { display: `6/3`, snellenNumerator: 6, snellenDenominator: 3, LogMAR: -0.30 },
   ]
 };
 
@@ -86,6 +88,8 @@ export class ObservationVisualAcuity {
    */
   private token: string;
 
+  private optotypesRead: number;
+
   /**
    * Constructor for initializing the FHIR server and token.
    *
@@ -95,6 +99,19 @@ export class ObservationVisualAcuity {
   constructor(fhirServer: string, token: string) {
     this.fhirServer = fhirServer;
     this.token = token;
+    this.optotypesRead = -2;
+  }
+
+  /**
+   * Converts Snellen's numerical aperture ratio (Snellen numerator/Snellen denominator)
+   * to Log MAR (Logarithmic Magnitude of Acuity Ratio).
+   *
+   * @param {number} snellenNumerator - The numerator of the Snellen aperture ratio.
+   * @param {number} snellenDenominator - The denominator of the Snellen aperture ratio.
+   * @return {number} The Logarithmic Magnitude of Acuity Ratio (Log MAR).
+   */
+  convertSnellenToLogMAR(snellenNumerator: number, snellenDenominator: number): number {
+    return Number(((Math.log10((snellenDenominator/snellenNumerator)))-(this.optotypesRead*0.02)).toFixed(1));
   }
 
   /**
